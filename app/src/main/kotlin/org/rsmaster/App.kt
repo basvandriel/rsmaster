02f3bd1727ecd69a37722a55542b0a3bb8f4edfd
Bas van Driel <contact@basvandriel.nl>
2020-11-14 22:22:56 +0100

    Kotlin setup for gradle

---
+++ app/src/main/kotlin/org/rsmaster/App.kt
@@ -0,0 +1,7 @@
+package org.rsmaster.app
+
+fun main(args: Array<String>) {
+    // val tokens = StringUtils.split(MessageUtils.getMessage())
+    // println(StringUtils.join(tokens))
+    println("Hello, world!")
+}
