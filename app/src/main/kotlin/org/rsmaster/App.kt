b1a1da345d7de8a76076ec710933d8638efc99b1
Bas van Driel <contact@basvandriel.nl>
2020-11-14 23:27:53 +0100

    Cleanup

--- app/src/main/kotlin/org/rsmaster/App.kt
+++
@@ -1,7 +0,0 @@
-package org.rsmaster.app
-
-fun main(args: Array<String>) {
-    // val tokens = StringUtils.split(MessageUtils.getMessage())
-    // println(StringUtils.join(tokens))
-    println("Hello, world!")
-}
---
+++ app/src/main/kotlin/org/rsmaster/Server.kt
@@ -0,0 +1,5 @@
+package org.rsmaster;
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
+}
