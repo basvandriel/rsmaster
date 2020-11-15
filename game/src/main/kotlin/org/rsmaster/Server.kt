81642624482848e79f74f88cbaa1629f2299e058
Bas van Driel <contact@basvandriel.nl>
2020-11-15 17:25:49 +0100

    app => game and time measurement

---
+++ game/src/main/kotlin/org/rsmaster/Server.kt
@@ -0,0 +1,33 @@
+package org.rsmaster
+
+/**
+* The server logger.
+*/
+private val logger = mu.KotlinLogging.logger {}
+
+/**
+ * The main function for initializing the game server
+ */
+fun main() {
+    var start = System.currentTimeMillis()
+
+    try {
+        val server = Server();
+        server.start();
+    } catch (e: Exception) {
+		System.exit(0);
+    }
+
+    logger.info { "Startup took ${System.currentTimeMillis() - start}ms" }
+}
+
+class Server() {
+    init { logger.info { "Starting up server..." } }
+
+    /**
+     * 
+     */
+    fun start() {
+        //...
+    }
+}
